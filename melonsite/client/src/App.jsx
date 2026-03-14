44a9f6d3aedac9fadbec25d1040540baf38965e2
aeropuerco <aeropuerco@gmail.com>
2026-03-14 01:04:00 +0100

    Inicio de repositorio. readme.md

---
+++ melonsite/client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <p>Aqui empieza todo...</p>
+        </a>
+      </div>
+      <h1>Cliente // Home</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
