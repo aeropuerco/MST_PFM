89fe5606b93672766148ae25da494d5832a81c48
aeropuerco <aeropuerco@gmail.com>
2026-05-07 11:16:01 +0200

    La descripción del commit anterior iba para el BACKEND. En el caso del FRONT-END, he modificado el componente de los PostItem en la home, para que muestren el autor correctamente (no salía)

--- melonsite/client/src/components/PostItem/PostItem.jsx
+++ melonsite/client/src/components/PostItem/PostItem.jsx
@@ -7,7 +7,7 @@ export const PostItem = ({id, title, author, exerpt, date}) => {
         <div className={postitemStyle.item} data-id={id}>
 
                     <div className={postitemStyle.title}>{title}</div>
-                    <div className={postitemStyle.exerpt}>EXERPT {exerpt}</div>
+                    <div className={postitemStyle.exerpt}>{exerpt}</div>
                     <div className={postitemStyle.date}>{author} /// {dateFormat(date)}</div>
                 
         </div>
